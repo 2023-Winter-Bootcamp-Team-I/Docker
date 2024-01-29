ed8e2ebb762ac26699e1c72621fd4d528de385a1
SeHoon <wer5136@naver.com>
2024-01-30 05:56:41 +0900

    feat: 공유페이지 수정

--- frontend/src/components/ShareModal.tsx
+++ frontend/src/components/ShareModal.tsx
@@ -1,6 +1,6 @@
 import React, { useState, ChangeEvent, useEffect } from 'react';
 import letterImg from '@/assets/images/letter.svg';
-import pencil from '@/assets/images/pencil.svg';
+import letter2 from '@/assets/images/letter2.svg';
 
 import { sendEmail } from '@/api/email';
 import { Link } from 'react-router-dom';
@@ -54,46 +54,42 @@ const ShareModal: React.FC<ShareModalProps> = ({ closeModal, bookId }) => {
           exit={{ opacity: 0 }}
           transition={{ duration: 0.5 }}
         >
-          <div className="w-[50%] h-[60%] bg-radial-gradient z-10 flex flex-col justify-center items-center">
-            <div className="bg-white mb-12 bg-opacity-30 mt-16 w-[55rem] h-[16rem] rounded-3xl flex flex-col">
-              <div className="flex flex-col justify-center items-center gap-4 -ml-8">
-                <div className="flex justify-center items-center gap-4 -ml-8">
-                  <img src={pencil} className="flex -mt-8 mb-12 scale-150" alt="연필 이미지" />
-                  <p className="font-jua text-[#659AFF] text-[2.7rem]">너의 동화책을 공유해봐!</p>
-                </div>
-                <div className="flex flex-col justify-center">
-                  <div className="flex justify-center">
-                    <img
-                      className="flex mx-auto min-h-full w-10  opacity-50 -mr-16 ml-10"
-                      src={letterImg}
-                      alt="letter_image"
-                    />
-                    <input
-                      id="email"
-                      name="email"
-                      type="email"
-                      placeholder="이메일을 입력해주세요"
-                      required
-                      value={email}
-                      onChange={handleEmailChange}
-                      className="block w-[100%] h-[4.2rem] text-base  rounded-full px-[8rem] py-[18px] border-2 border-shadowGray focus:outline-signupButtonBlue"
-                    />
-                  </div>
-                  <div className="flex mb-[7vh] justify-center"></div>
-                  <div className="flex flex-row gap-2 ml-2 justify-center">
-                    <Link to="/library">
-                      <button
-                        onClick={() => {
-                          handleShareButtonClick();
-                          closeModal();
-                        }}
-                        className="w-[15rem] h-[4.5rem] text-[#EEEEEE] bg-[#659AFF] pt-2 text-[2rem] rounded-3xl border-[#5483DC] border-b-8 border-r-4 hover:bg-[#508dff]"
-                      >
-                        공유하기
-                      </button>
-                    </Link>
-                  </div>
-                </div>
+          <div className="bg-signupButtonBlue mb-12 bg-opacity-30 mt-16 w-[55rem] h-[25rem] rounded-3xl flex flex-col z-20 justify-between">
+            <div className="flex flex-col justify-center items-center -ml-8">
+              <div className="flex justify-center items-center gap-4 -ml-28">
+                <img src={letter2} className="flex -mt-8 mb-12 mr-10 scale-150" />
+                <p className="font-jua mt-12 text-white text-[2.7rem]">너의 동화책을 공유해봐!</p>
+              </div>
+              <div className="flex justify-center mt-16 ">
+                <img
+                  className="flex mx-auto min-h-full w-10  opacity-50 -mr-16 ml-10"
+                  src={letterImg}
+                  alt="letter_image"
+                />
+                <input
+                  id="email"
+                  name="email"
+                  type="email"
+                  placeholder="이메일을 입력해주세요"
+                  required
+                  value={email}
+                  onChange={handleEmailChange}
+                  className="block w-[100%] h-[4.2rem] text-lg  rounded-full px-[8rem] py-[18px] border-2 border-shadowGray focus:outline-signupButtonBlue"
+                />
+              </div>
+              <div className="flex mb-[13vh] justify-center"></div>
+              <div className="flex flex-row gap-2 ml-2 justify-center -mt-16">
+                <Link to="/library">
+                  <button
+                    onClick={() => {
+                      handleShareButtonClick();
+                      closeModal();
+                    }}
+                    className="w-[15rem] h-[4.5rem] text-[#EEEEEE] bg-[#659AFF] pt-2 text-[2rem] rounded-3xl border-[#5483DC] border-b-8 border-r-4 hover:bg-[#508dff] font-jua"
+                  >
+                    공유하기
+                  </button>
+                </Link>
               </div>
             </div>
           </div>
