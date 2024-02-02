0608aa5615525d675ba9f5abbeba27bb5790e026
KimJinYong <kjy154969@gmail.com>
2024-02-02 14:12:42 +0900

    [bugfix/#86] tts 영어 수정

--- backend/book/tasks.py
+++ backend/book/tasks.py
@@ -38,48 +38,87 @@ def generate_dalle_image_async(image_uuid, enContent):
 @shared_task
 def gtts_async(tts_uuid, content, lan):
     print("진입")
-
-    # 네이버 API 사용을 위한 설정 (클라이언트 ID와 시크릿)
-    client_id = get_secret("NAVER_CLIENT_ID")  # 네이버 클라우드 플랫폼에서 발급받은 클라이언트 ID
-    client_secret = get_secret("NAVER_CLIENT_SECRET") # 네이버 클라우드 플랫폼에서 발급받은 클라이언트 시크릿
-
-    # 변환할 텍스트 인코딩
-    encText = urllib.parse.quote(content)
-
     if lan == 'ko':
+        # 네이버 API 사용을 위한 설정 (클라이언트 ID와 시크릿)
+        client_id = get_secret("NAVER_CLIENT_ID")  # 네이버 클라우드 플랫폼에서 발급받은 클라이언트 ID
+        client_secret = get_secret("NAVER_CLIENT_SECRET") # 네이버 클라우드 플랫폼에서 발급받은 클라이언트 시크릿
+
+        # 변환할 텍스트 인코딩
+        encText = urllib.parse.quote(content)
+
+
         # 네이버 음성합성 API 요청 데이터 설정
         data = "speaker=nshasha&volume=0&speed=0&pitch=0&format=mp3&text=" + encText
-    elif lan == 'en':
         # 네이버 음성합성 API 요청 데이터 설정
         data = "speaker=dara-danna&volume=0&speed=0&pitch=-3&format=mp3&text=" + encText
 
-    # 네이버 음성합성 API URL
-    url = "https://naveropenapi.apigw.ntruss.com/tts-premium/v1/tts"
+        # 네이버 음성합성 API URL
+        url = "https://naveropenapi.apigw.ntruss.com/tts-premium/v1/tts"
 
-    # HTTP 요청 생성 및 헤더 설정
-    request = urllib.request.Request(url)
-    request.add_header("X-NCP-APIGW-API-KEY-ID", client_id)
-    request.add_header("X-NCP-APIGW-API-KEY", client_secret)
+        # HTTP 요청 생성 및 헤더 설정
+        request = urllib.request.Request(url)
+        request.add_header("X-NCP-APIGW-API-KEY-ID", client_id)
+        request.add_header("X-NCP-APIGW-API-KEY", client_secret)
 
-    # HTTP 요청 전송 및 응답 받기
-    response = urllib.request.urlopen(request, data=data.encode('utf-8'))
-    rescode = response.getcode()
+        # HTTP 요청 전송 및 응답 받기
+        response = urllib.request.urlopen(request, data=data.encode('utf-8'))
+        rescode = response.getcode()
 
-    # 응답 처리
-    if rescode == 200:
-        temp_file_path = f"{tts_uuid}.mp3"
+        # 응답 처리
+        if rescode == 200:
+            temp_file_path = f"{tts_uuid}.mp3"
 
-        with open(temp_file_path, 'wb') as file_data:
-            file_data.write(response.read())
+            with open(temp_file_path, 'wb') as file_data:
+                file_data.write(response.read())
 
-        with open(temp_file_path, 'rb') as file_data:
-            url = upload_to_s3(tts_uuid, file_data, 'tts')
+            with open(temp_file_path, 'rb') as file_data:
+                url = upload_to_s3(tts_uuid, file_data, 'tts')
 
-        os.remove(temp_file_path)
-        return url
-    else:
-        print("Error Code:" + rescode)
-        return None
+            os.remove(temp_file_path)
+            return url
+        else:
+            print("Error Code:" + rescode)
+            return None
+    elif lan == 'en':
+        # 네이버 API 사용을 위한 설정 (클라이언트 ID와 시크릿)
+        client_id = get_secret("NAVER_CLIENT_ID")  # 네이버 클라우드 플랫폼에서 발급받은 클라이언트 ID
+        client_secret = get_secret("NAVER_CLIENT_SECRET")  # 네이버 클라우드 플랫폼에서 발급받은 클라이언트 시크릿
+
+        # 변환할 텍스트 인코딩
+        encText = urllib.parse.quote(content)
+
+        # 네이버 음성합성 API 요청 데이터 설정
+        data = "speaker=nshasha&volume=0&speed=0&pitch=0&format=mp3&text=" + encText
+        # 네이버 음성합성 API 요청 데이터 설정
+        data = "speaker=dara-danna&volume=0&speed=0&pitch=-3&format=mp3&text=" + encText
+
+        # 네이버 음성합성 API URL
+        url = "https://naveropenapi.apigw.ntruss.com/tts-premium/v1/tts"
+
+        # HTTP 요청 생성 및 헤더 설정
+        request = urllib.request.Request(url)
+        request.add_header("X-NCP-APIGW-API-KEY-ID", client_id)
+        request.add_header("X-NCP-APIGW-API-KEY", client_secret)
+
+        # HTTP 요청 전송 및 응답 받기
+        response = urllib.request.urlopen(request, data=data.encode('utf-8'))
+        rescode = response.getcode()
+
+        # 응답 처리
+        if rescode == 200:
+            temp_file_path = f"{tts_uuid}.mp3"
+
+            with open(temp_file_path, 'wb') as file_data:
+                file_data.write(response.read())
+
+            with open(temp_file_path, 'rb') as file_data:
+                url = upload_to_s3(tts_uuid, file_data, 'tts')
+
+            os.remove(temp_file_path)
+            return url
+        else:
+            print("Error Code:" + rescode)
+            return None
 
     # print("진입")
     # tts = gTTS(text=content, lang=lan)
